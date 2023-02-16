46f33c986219523acdd0a7a55a8d9f2b15c4e584
AndoGhevian <lenyvonbeer@gmail.com>
2023-02-16 12:32:20 +0400

    Finish functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React, { PropsWithChildren, PropsWithRef, forwardRef, useEffect, useRef, useLayoutEffect, useState } from 'react';
-import './App.css';
+import { animated, useSpring } from '@react-spring/web';
+import { useDrag, useGesture } from '@use-gesture/react';
+import React, {
+  PropsWithRef,
+  forwardRef,
+  useEffect,
+  useRef,
+  useLayoutEffect,
+  useState,
+} from 'react';
 import usePrevValue from './hooks/usePrevValue';
+import './App.css';
 
 function App() {
   return (
@@ -8,10 +17,13 @@ function App() {
       Zoomable Slider
       <div>
         <ZoomableSlider
-          step={10}
-          max={100}
-          value={85}
-        // width={100}
+          step={3}
+          max={10}
+          // value={85}
+          width={700}
+          // discrete
+          // fixToStep
+          onChange={(val, finish) => console.log(val, finish)}
         />
       </div>
     </div>
@@ -22,14 +34,15 @@ type ZoomableSliderProps = {
   className?: string;
   step: number;
   max: number;
-  value: number; // make this as in two way binding
+  value?: number; // make this as in two way binding
   discrete?: boolean;
+  fixToStep?: boolean;
   /**
    * Need to pass this property when you want to handle resize effects
   */
   width?: number;
   mapLabel?: (...props: any) => any
-  onChange?: (value: number) => void
+  onChange?: (value: number, finish: boolean) => void
 }
 
 const ZoomableSlider = forwardRef(function ZoomableSlider(props: PropsWithRef<ZoomableSliderProps>, ref: any) {
@@ -39,12 +52,18 @@ const ZoomableSlider = forwardRef(function ZoomableSlider(props: PropsWithRef<Zo
     max,
     value,
     width,
+    onChange = () => void 0,
+    discrete = false,
+    fixToStep = false,
     mapLabel,
   } = props
   const sliderRef = useRef<HTMLDivElement>(null)
-  const prevRef = usePrevValue(ref)
+  const [rangeValue, setRangeValue] = useState(0)
   const [lastFixedWidth, setLastFixedWidth] = useState<number>()
 
+  const lastChangeValueRef = useRef<number | null>(value || rangeValue)
+
+  const prevRef = usePrevValue(ref)
   useEffect(() => {
     if (prevRef && prevRef !== ref) {
       if (typeof ref === 'function') {
@@ -83,6 +102,109 @@ const ZoomableSlider = forwardRef(function ZoomableSlider(props: PropsWithRef<Zo
     }
   })
 
+  const [pointerSpring, pointerSpringApi] = useSpring(
+    () => ({
+      from: { left: 0 },
+      // to: { left: 0 },
+    }),
+    []
+  )
+
+  // Use this because inability to call setState from useDrag hook.
+  const bind = useGesture({
+    onDrag: ({ down, offset }) => {
+      const pointerPureOffsetX = offset[0]
+      const offsetX = pointerPureOffsetX * max / currentWidth!
+
+      const microStepValue = Math.round(offsetX)
+      let newValue = microStepValue
+      const endStepIndex = labels.findIndex(({ offset }) => newValue <= offset)
+
+      if (fixToStep) {
+        if (endStepIndex !== 0) {
+          newValue = Math.abs(
+            offsetX - labels[endStepIndex].offset
+          ) > Math.abs(
+            offsetX - labels[endStepIndex - 1].offset
+          )
+            ? labels[endStepIndex - 1].offset
+            : labels[endStepIndex].offset
+        } else {
+          newValue = labels[endStepIndex].offset
+        }
+      }
+      const newValueOffsetX = newValue * currentWidth! / max
+      const microStepOffsetX = microStepValue * currentWidth! / max
+
+      if (!discrete) {
+        if (down) {
+          pointerSpringApi.start({ left: pointerPureOffsetX })
+        } else {
+          pointerSpringApi.start({ left: newValueOffsetX, })
+        }
+      } else {
+        if (down) {
+          pointerSpringApi.start({
+            left: microStepOffsetX,
+            immediate: true,
+            // onChange: () => onChange(microStepValue, down)
+          })
+        } else {
+          pointerSpringApi.start({ left: newValueOffsetX, immediate: true })
+        }
+      }
+
+      const finished = !down
+      if (down) {
+        if (lastChangeValueRef.current !== microStepValue) {
+          onChange(microStepValue, finished)
+        }
+        lastChangeValueRef.current = microStepValue
+      } else {
+        onChange(newValue, finished)
+        setRangeValue(newValue)
+        lastChangeValueRef.current = newValue
+      }
+    }
+  }, {
+    drag: {
+      from: (_) => [pointerSpring.left.get(), 0],
+      axis: 'x', bounds: (gestureState) => {
+        if (gestureState) {
+          const { target } = gestureState
+          // const centeringOffset = (target as HTMLElement).offsetHeight / 2
+          return {
+            left: 0,
+            right: sliderRef.current!.clientWidth /* - centeringOffset */
+          }
+        }
+        return { left: 0, right: 0 }
+      }
+    }
+  })
+
+  const prevValue = usePrevValue(value)
+
+  useEffect(() => {
+    let newValue: number
+    if (value) {
+      newValue = value
+    } else {
+      newValue = rangeValue
+      if (value === undefined && prevValue !== undefined) {
+        setRangeValue(prevValue)
+      }
+    }
+    lastChangeValueRef.current = newValue
+
+    const newValueOffsetX = currentWidth ? newValue * currentWidth / max : 0
+    if (!discrete) {
+      pointerSpringApi.start({ left: newValueOffsetX })
+    } else {
+      pointerSpringApi.start({ left: newValueOffsetX, immediate: true })
+    }
+  }, [rangeValue, value])
+
   return (
     <div
       className={className ? `${className} ZoomableSlider-Slider` : "ZoomableSlider-Slider"}
@@ -102,12 +224,16 @@ const ZoomableSlider = forwardRef(function ZoomableSlider(props: PropsWithRef<Zo
           </div>
         ))}
       </div>
-      <div
-        className="ZoomableSlider-Pointer Triangle"
+      <animated.div
+        {...bind()}
+        className="ZoomableSlider-PointerContainer"
         style={{
-          left: currentWidth ? value * currentWidth / max : 0
+          // left: currentWidth ? rangeValue * currentWidth / max : 0,
+          ...pointerSpring,
         }}
-      ></div>
+      >
+        <div className="ZoomableSlider-Pointer Triangle"></div>
+      </animated.div>
     </div>
   )
 })
